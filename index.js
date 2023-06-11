53f82fc0f54605f11b5b3612ceaaa4c8ca51ba08
nahid139 <nahidnhptake@gmail.com>
2023-06-11 13:22:43 -0700

    user server added

--- index.js
+++ index.js
@@ -40,12 +40,21 @@ async function run() {
 
     // users
 
-    // app.post("/users", async (req, res) => {
-    //   const user = req.body;
-    //   console.log(user);
-    //   const result = await usersCollection.insertOne(user);
-    //   res.send(result);
-    // });
+    app.get('/users',  async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
 
 
